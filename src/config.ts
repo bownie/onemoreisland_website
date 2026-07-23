eb79996b661436fb2b7f74b498846162e13ce874
Richard Bown <rich@xyglo.com>
2026-07-23 17:00:37 +0200

    another fix

--- src/config.ts
+++ src/config.ts
@@ -14,6 +14,7 @@ const config: ResolvedAstroPaperConfig = {
   site: {
     ...userConfig.site,
     ogImage: userConfig.site.ogImage ?? DEFAULT_OG_IMAGE,
+    shortTitle: userConfig.site.shortTitle ?? userConfig.site.title,
     lang: userConfig.site.lang ?? "en",
     timezone: userConfig.site.timezone ?? "UTC",
     dir: userConfig.site.dir ?? "ltr",
